66659670ff7c3d21a2ff0c86cea2582c4ae1d916
sajede <sajijahan137726@gmail.com>
2022-04-03 21:27:02 +0430

    postgres connection

--- projectA/projectA/settings.py
+++ projectA/projectA/settings.py
@@ -66,13 +66,17 @@ WSGI_APPLICATION = 'projectA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'masjed',
+        'USER': 'postgres',
+        'PASSWORD': '9054609',
+        'HOST': '127.0.0.1',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
